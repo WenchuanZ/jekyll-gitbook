680f728478460265c17849993bca7381584be5d2
Wenchuan <wzhu8806@gmail.com>
2023-02-11 20:46:53 +1100

    Update app.js

--- particles.js-master/demo/js/app.js
+++ particles.js-master/demo/js/app.js
@@ -16,7 +16,7 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 20,
+        "value": 10,
         "density": {
           "enable": true,
           "value_area": 800
@@ -86,7 +86,7 @@ particlesJS('particles-js',
       "events": {
         "onhover": {
           "enable": true,
-          "mode": "repulse"
+          "mode": "grab"
         },
         "onclick": {
           "enable": true,
